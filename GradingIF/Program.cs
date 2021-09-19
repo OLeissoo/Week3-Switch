88d695ec54330a0d48be978d583806894f9b84ab
OliverLeissoo <oleissoo@tktk.ee>
2021-09-19 19:41:02 +0300

    add new projects

---
+++ GradingIF/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace GradingIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajalt, mis hinde ta sai
+            //Kui kasutaja sai "A", konsool kuvab "Suurepärane!";
+            //Kui 'B', konsool kuvab "Väga hea!";
+            //Kui 'C', "Hea!";
+            //Kui 'D', "Rahuldav";
+            //Kui 'E', "Kasin";
+            //Kui 'F', "Puudulik".
+            //Kui kasutaja sisetab midagi muud, kuva "Vale väärtus."
+
+            //KASUTAME IF
+
+            Console.WriteLine("Sisesta oma tulemus:");
+            //char - character - salvestab ainult 1 sümboli
+            char userResult = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            if (userResult == 'A')
+            {
+                Console.WriteLine("Suurepärane!");
+            }
+            else if (userResult == 'B')
+            {
+                Console.WriteLine("Väga hea!");
+            }
+            else if (userResult == 'C')
+            {
+                Console.WriteLine("Hea!");
+            }
+            else if (userResult == 'D')
+            {
+                Console.WriteLine("Rahuldav");
+            }
+            else if (userResult == 'E')
+            {
+                Console.WriteLine("Kasin");
+            } 
+            else if (userResult == 'F')
+            {
+                Console.WriteLine("Puudulik");
+            }
+            else
+            {
+                Console.WriteLine("Vale väärtus.");
+            }
+        }
+    }
+}
---
+++ GradingSwitch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace GradingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajalt, mis hinde ta sai
+            //Kui kasutaja sai "A", konsool kuvab "Suurepärane!";
+            //Kui 'B', konsool kuvab "Väga hea!";
+            //Kui 'C', "Hea!";
+            //Kui 'D', "Rahuldav";
+            //Kui 'E', "Kasin";
+            //Kui 'F', "Puudulik".
+            //Kui kasutaja sisetab midagi muud, kuva "Vale väärtus."
+
+            //KASUTAME SWITCH
+
+            Console.WriteLine("Sisesta oma tulemus:");
+            //char - character - salvestab ainult 1 sümboli
+            char userResult = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            switch (userResult)
+            {
+                case 'A':
+                    Console.WriteLine("Suurepärane!");
+                    break;
+                case 'B':
+                    Console.WriteLine("Väga hea!");
+                    break;
+                case 'C':
+                    Console.WriteLine("Hea!");
+                    break;
+                case 'D':
+                    Console.WriteLine("Rahuldav");
+                    break;
+                case 'E':
+                    Console.WriteLine("Kasin");
+                    break;
+                case 'F':
+                    Console.WriteLine("Puudulik");
+                    break;
+                default:
+                    Console.WriteLine("Vale väärtus.");
+                    break;
+            }
+        }
+    }
+}
